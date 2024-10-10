ac40cde4eddd754f5a9b45781d1934b40d6e1464
zachuri <punsalang.zachary@gmail.com>
2024-10-09 23:50:19 -0700

    feat: updated display to be able to display next and prev pokemons

--- src/components/Pokemons.tsx
+++ src/components/Pokemons.tsx
@@ -1,25 +1,30 @@
 "use client";
 
-import { EBackend } from "@/consts/api";
+import { API_URL } from "@/consts/api";
 import { IPokemon, IPokemonBaseUrl } from "@/types/pokemon";
 import { memo, useEffect, useState } from "react";
 import { PokemonCard } from "./pokemon/PokemonCard";
 import { PokemonLayout } from "./pokemon/PokemonLayout";
+import { Button } from "./ui/button";
 import { Loading } from "./ui/loading";
 
 export const Pokemons = memo(function Pokemons() {
+	const [apiUrl, setApiUrl] = useState<string | null>(API_URL);
 	const [pokemons, setPokemons] = useState<IPokemon[] | null>();
+	const [next, setNext] = useState<string | null>(null);
+	const [previous, setPrevious] = useState<string | null>(null);
 
 	useEffect(() => {
 		async function fetchPokemon() {
 			try {
-				const response = await fetch(EBackend.API_URL);
+				const response = await fetch(apiUrl || "");
 
 				if (!response.ok) {
 					throw new Error(`Response status: ${response.status}`);
 				}
 
 				const json: IPokemonBaseUrl = await response.json();
+
 				console.log(json);
 
 				// Map through the results and add the id
@@ -31,12 +36,22 @@ export const Pokemons = memo(function Pokemons() {
 					}) || [];
 
 				setPokemons(pokemonsWithId);
+				setNext(json.next);
+				setPrevious(json.previous);
 			} catch (error) {
 				console.error(error);
 			}
 		}
 		fetchPokemon();
-	}, []);
+	}, [apiUrl]); // Remove next and previous from dependencies
+
+	function handleNext() {
+		if (next) setApiUrl(next);
+	}
+
+	function handlePrevious() {
+		if (previous) setApiUrl(previous);
+	}
 
 	return (
 		<>
@@ -47,6 +62,14 @@ export const Pokemons = memo(function Pokemons() {
 					{pokemons.map(pokemon => (
 						<PokemonCard key={pokemon.id} {...pokemon} />
 					))}
+					<div className='flex col-span-4 items-center justify-center gap-4'>
+						<Button onClick={handlePrevious} disabled={!previous}>
+							Previous
+						</Button>
+						<Button onClick={handleNext} disabled={!next}>
+							Next
+						</Button>
+					</div>
 				</PokemonLayout>
 			)}
 		</>
--- src/consts/api.ts
+++ src/consts/api.ts
@@ -1,3 +1,1 @@
-export const enum EBackend {
-  API_URL = "https://pokeapi.co/api/v2/pokemon"
-}
+export const API_URL = "https://pokeapi.co/api/v2/pokemon";
