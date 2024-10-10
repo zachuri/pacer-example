a34b67320f110481732f6e152f261014db3be888
zachuri <punsalang.zachary@gmail.com>
2024-10-09 23:30:21 -0700

    feat: created reusable component for pokemon card

---
+++ src/components/pokemon/PokemonCard.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { IPokemon } from "@/types/pokemon";
+import Image from "next/image";
+import Link from "next/link";
+import { Card } from "../ui/card";
+
+export function PokemonCard({ id, name, url }: IPokemon) {
+	console.log(id, name, url);
+	return (
+		<Card className='flex flex-col justify-center items-center'>
+			<Link href={url}>
+				<Image
+					alt={`${name}`}
+					width={100}
+					height={100}
+					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+				/>
+			</Link>
+			<h1 className='capitalize'>{name}</h1>
+		</Card>
+	);
+}
